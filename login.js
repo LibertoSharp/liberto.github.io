44f84e2461865037deee645bc18b17d574c2a297
LibertoSharp <109512352+LibertoSharp@users.noreply.github.com>
2024-06-06 17:38:24 +0200

    Add files via upload

---
+++ login.js
@@ -0,0 +1,26 @@
+// Funzione per controllare le credenziali
+function checkCredentials() {
+  var username = document.querySelector('input[name="Username"]').value;
+  var password = document.querySelector('input[name="Password"]').value;
+
+  // Carica il file JSON corrispondente all'username
+  fetch(username + '.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Username non trovato');
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.password !== password) {
+        throw new Error('Password errata');
+      }
+      alert('Login riuscito');
+    })
+    .catch(error => {
+      alert(error.message);
+    });
+}
+
+// Aggiunge un gestore per l'evento click al bottone di login
+document.getElementById('wrapper').addEventListener('click', checkCredentials);
