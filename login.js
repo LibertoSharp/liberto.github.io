9dedcdc22f1b8f9056b24eaeea26d41296712155
Libeeee <Lucavverato2005luca@gmail.com>
2024-06-07 15:45:36 +0200

    Auto Login

--- login.js
+++ login.js
@@ -28,6 +28,34 @@ function checkCredentials() {
     });
 }
 
+function checkCredentials(username, password) {
+  if (username == "") {
+    attivaScritta("Credenziali Invalide");
+    return;
+  }
+
+  // Carica il file JSON corrispondente all'username
+  fetch(username + '.json')
+    .then(response => {
+      if (!response.ok) {
+        attivaScritta("Username non trovato");
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.password !== password) {
+        attivaScritta("Password Errata");
+        return;
+      }
+      setCookie("Username", username, 60);
+      setCookie("Password", password, 60);
+      caricaPagina("main");
+    })
+    .catch(error => {
+      attivaScritta(error.message);
+    });
+}
+
 function caricaPagina(nome) {
   window.location.href = nome+".html";
 }
@@ -42,5 +70,32 @@ function attivaScritta(msg) {
   }, 4000);
 }
 
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
 // Aggiunge un gestore per l'evento click al bottone di login
 document.getElementById('wrapper').addEventListener('click', checkCredentials);
+
+username = getCookie("Username");
+password = getCookie("Password");
+
+if (username !== "" && password !== "")
+  checkCredentials(username, password);
