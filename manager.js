e95eed6d5d21497312e206f5d7edc74666162e00
Libeeee <Lucavverato2005luca@gmail.com>
2024-06-09 15:29:08 +0200

    Update 9 fix EXP update fix fix fix fix fix debug fix fix definitivo questa volta per davvero o forse no ora davvero

--- manager.js
+++ manager.js
@@ -58,7 +58,7 @@ async function getFieldValueFromJSON(fieldName) {
 
       response.json().then(jsonfile => 
         {
-          field = jsonfile.EXP;
+          field = JSON.parse(jsonfile).EXP;
         });
     })
 
